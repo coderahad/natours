cae587057baa0721c4c6669ed2c057a35e3996cc
ahad hossain <hossain1814@gmail.com>
2020-04-26 12:21:21 +0600

    importing data from tours-simple json and alias routes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,66 @@
 const Tour = require('../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,duration';
+  next();
+};
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUILD QUERY
+    //1) FILTERING
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'limit', 'sort', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    //2) ADVANCE FILTERING
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(
+      /\b(gt|gte|lt|lte)\b/g,
+      match => `$${match}`
+    );
+    // {duration: {$gte: 5}, difficulty: 'easy'}
+    // { duration: { gte: '5' }, difficulty: 'easy' }
+
+    let query = Tour.find(JSON.parse(queryString));
+    // .where('duration')
+    // .equals(5)
+    // .where('difficulty')
+    // .equals('easy');
+    // SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+      //sort('price ratingsAverage')
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+      // select('name price ratingsAverage duration')
+    } else {
+      query = query.select('-__v');
+    }
+
+    //PAGINATION
+
+    const limit = req.query.limit * 1 || 100;
+    const page = req.query.page * 1 || 1;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'Success',
       result: tours.length,
