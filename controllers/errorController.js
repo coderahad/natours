cbd03e6d825fea70dd6188e0765c09a88c837884
ahad hossain <hossain1814@gmail.com>
2020-05-07 11:59:39 +0600

    handling unhandled rejections like database connection fail

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -14,7 +14,7 @@ const handleValidationErrorDB = err => {
   const errors = Object.values(err.errors).map(el => el.message);
   const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
-}
+};
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -55,7 +55,8 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
     sendErrorProd(error, res);
   }
 };
