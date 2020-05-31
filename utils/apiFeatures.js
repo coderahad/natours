733c68a077aed2f647e14ed0e79dff8d4a5c2336
ahad hossain <hossain1814@gmail.com>
2020-05-31 20:54:03 +0600

    embade data to tour-model, geospatial data, geoJSON data, section 11 starts here

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -25,7 +25,6 @@ class ApiFeatures {
 
   sort() {
     if (this.queryString.sort) {
-      console.log(this.queryString.sort);
       const sortBy = this.queryString.sort.split(',').join(' ');
       this.query = this.query.sort(sortBy);
       //sort('price ratingsAverage')
