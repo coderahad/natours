d1bb2deb1b398869448f155f570a6ebfac555f4d
ahad hossain <hossain1814@gmail.com>
2020-06-04 17:49:24 +0600

    review model, controller, routes, route mounting in app.js task done

---
+++ models/reviewModel.js
@@ -0,0 +1,39 @@
+// review / rating / createdAt / ref to Tour / ref to User
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      reqiured: [true, "Review can't be empty"]
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+      max: [5.0, 'Rating must be less than or equal to 5'],
+      min: [1.0, 'Rating must be greater than or equal to 1']
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour']
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user']
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
