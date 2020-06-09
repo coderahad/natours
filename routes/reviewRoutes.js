3e0e1e92592eb58b8622b8284222a69713d27ce1
ahad hossain <hossain1814@gmail.com>
2020-06-09 18:24:07 +0600

    authenticate and authorize routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,15 +4,12 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
-// POST /tours/249d5f/reviews
-// GET /tours/249d5f/reviews
-// POST /reviews
+router.use(authController.protect);
 
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
@@ -21,7 +18,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('admin', 'user'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('admin', 'user'),
+    reviewController.deleteReview
+  );
 
 module.exports = router;
