eb19988e4845d3dae9e9f2ff7bd7239a9e94bf00
ahad hossain <hossain1814@gmail.com>
2020-06-25 02:23:15 +0600

    map automatically figure out which portion of the map it should display in order to fit all the current locations

--- public/js/mapBox.js
+++ public/js/mapBox.js
@@ -6,8 +6,26 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYWhhZGhvc3NhaW4iLCJhIjoiY2tidDBjdDk1MDU4YjJxb
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/ahadhossain/ckbt1bkr00gev1iohohavw2hw',
-    center: [ -118.214010,33.981452],
-    zoom: 10,
-    interactive: false
+    // center: [ -118.214010,33.981452],
+    // zoom: 10,
+    // interactive: false
 });
 
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+    // 1) Create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
+
+    // 2) Add marker
+    new mapboxgl.Marker({
+        element: el,
+        anchor: 'bottom'
+    }).setLngLat(loc.coordinates).addTo(map);
+    
+    // 3) Extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+})
+
+map.fitBounds(bounds);
