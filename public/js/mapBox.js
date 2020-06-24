1e4e7cb4f7828583e660c322baa8f7b144058352
ahad hossain <hossain1814@gmail.com>
2020-06-24 14:18:54 +0600

    store location data in map id, collect locations data from html and create object from it instead make an api call for locationsof a tour.created mapbox account, use mapbox in our app. created a mapbox style for my own use and then use that style to my style url

---
+++ public/js/mapBox.js
@@ -0,0 +1,10 @@
+/*eslint-disable*/
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWhhZGhvc3NhaW4iLCJhIjoiY2tidDBjdDk1MDU4YjJxbnN6aTQ4N3JtZiJ9.eK0gnNWh8Xq2j1WyYpVyZQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/ahadhossain/ckbt1bkr00gev1iohohavw2hw'
+});
+
