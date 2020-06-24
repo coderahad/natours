fbdb535f0920999d883ad6c9652c8984ec5e7936
ahad hossain <hossain1814@gmail.com>
2020-06-24 17:47:55 +0600

    mapbox options for which portion of map can be seen. decide center, zoom, interactivity from mapbox documentation

--- public/js/mapBox.js
+++ public/js/mapBox.js
@@ -5,6 +5,9 @@ console.log(locations);
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWhhZGhvc3NhaW4iLCJhIjoiY2tidDBjdDk1MDU4YjJxbnN6aTQ4N3JtZiJ9.eK0gnNWh8Xq2j1WyYpVyZQ';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/ahadhossain/ckbt1bkr00gev1iohohavw2hw'
+    style: 'mapbox://styles/ahadhossain/ckbt1bkr00gev1iohohavw2hw',
+    center: [ -118.214010,33.981452],
+    zoom: 10,
+    interactive: false
 });
 
