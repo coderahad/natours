b7615ab5ce87cd05d18728c3ea1d4b1bf0fd3fe4
ahad hossain <hossain1814@gmail.com>
2020-07-14 18:23:46 +0600

    image processing with sharp(resize, fileformat, file-destination). use memory storage instead of diskStorage

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,24 @@
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/img/users');
-  },
-  filename: (req, file, cb) => {
-    // user-12454fd545f4-2314564788.jpg
-    // first 'user' then 'userId' then 'timeStamp'. if same user upload several photos then timestamp prevents photo overwrite with the same user id. and if different user uploads
-    // differnt photos at the same time then userId prevents duplicate photo error(photos with same name).
-    const ext = file.mimetype.split('/')[1];
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  }
-});
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     // user-12454fd545f4-2314564788.jpg
+//     // first 'user' then 'userId' then 'timeStamp'. if same user upload several photos then timestamp prevents photo overwrite with the same user id. and if different user uploads
+//     // differnt photos at the same time then userId prevents duplicate photo error(photos with same name).
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   }
+// });
+
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
@@ -28,6 +31,18 @@ const multerFilter = (req, file, cb) => {
 const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
 
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+};
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
