cc35e53b01962c7dd473a3f0221a706bd7ca82ed
ahad hossain <hossain1814@gmail.com>
2020-07-21 14:15:24 +0600

    email templates with pug. send welcome email when user sign up

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -46,6 +46,10 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordChangedAt: req.body.passwordChangedAt,
     role: req.body.role
   });
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  await new Email(newUser, url).sendWelcome();
+
   createSendToken(newUser, 201, res);
 });
 
@@ -174,11 +178,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
   const message = `Forget your password? please submit a patch request with your new password and passwordConfirm to: ${resetURL}. \n If you didn't forget your password then ignore this message.`;
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token valid for 10 minutes',
-      message
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Your password reset token valid for 10 minutes',
+    //   message
+    // });
 
     res.status(200).json({
       status: 'success',
