cc7af369332f9704d8812365bea678a1863be055
ahad hossain <hossain1814@gmail.com>
2020-07-24 19:56:09 +0600

    first commit of stripe payment process.set up the backend server for stripe. create a checkout session from the stripe server with the tour id

---
+++ routes/bookingRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
+
+const router = express.Router();
+
+router.get(
+  '/checkout-session/:tourId',
+  authController.protect,
+  bookingController.getCheckoutSession
+);
+
+module.exports = router;
